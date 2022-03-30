5b3b0f6e0bf3d5b408967ab2bbccbbb1dd62e7ed
Petr Porobov <porobov.p@gmail.com>
2022-03-30 12:17:42 +0400

    Explode function added

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const { UpalaConstants } = require('@upala/constants')
 // newIdentityOwner - address of the new id owner
 // wallet - ethersjs wallet object
 // upalaConstants - upala constants object 
-async function newIdentity(newIdentityOwner, wallet, upalaConstants) {
 
-    const upala = upalaConstants.getContract('Upala', wallet)
+async function loadUpalaconstants(upalaConstants) {
+    if (upalaConstants) {
+        // mainly needed for tests (test don't load constants from disk)
+        return upalaConstants
+    } else {
+        // for all other clients of group-manager upConstants is built in
+        return new UpalaConstants(await wallet.getChainId())
+    }
+}
+async function newIdentity(newIdentityOwner, wallet, upalaConstants) {
+    let upConsts = loadUpalaconstants(upalaConstants)
+    const upala = upConsts.getContract('Upala', wallet)
     const tx = await upala.connect(wallet).newIdentity(newIdentityOwner)
   
     // retrieve id (todo - is there an easier way?)
@@ -12,6 +23,25 @@ async function newIdentity(newIdentityOwner, wallet, upalaConstants) {
     const events = await upala.queryFilter(eventFilter, blockNumber, blockNumber)
     const upalaID = events[0].args.upalaId
     return upalaID
-  }
-
-  module.exports = { newIdentity }
+}
+// attackPayload - pool-specific attack payload
+async function explode(wallet, poolAddress, attackPayload, upalaConstants) {
+    // get pool factory by poolAddress
+    // load abi and explosion instructions
+    // check data
+    // explode
+    let upConsts = loadUpalaconstants(upalaConstants)
+    let { score, bundleId, proof } = attackPayload
+    // todo check if id already exists for the address
+    const upalaID = await newIdentity(wallet.address, wallet)
+    // const upala = upConsts.getContract('Upala', wallet)
+    const poolType = 'SignedScoresPool'  // hard-coded for now
+    const pool = upConsts.getContract(poolType, wallet, poolAddress)
+    return await pool.connect(wallet).attack(
+        upalaID, 
+        wallet.address,  // scoreAssignedTo
+        score,
+        bundleId,
+        proof)
+}
+module.exports = { newIdentity, explode }
